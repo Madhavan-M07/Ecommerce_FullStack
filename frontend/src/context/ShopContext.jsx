45efd4e2cf44c0f2f873765b5fe513f77f21dfd1
Madhavan-M07 <madhavan09072003@gmail.com>
2026-01-11 22:35:33 +0530

    feat:cart total amount logic

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -12,13 +12,10 @@ const ShopContextProvider = (props) => {
   const [showSearch, setShowSearch] = useState(false);
   const [cartItems, setCartItems] = useState({});
 
-
-
-  const addToCart = async (itemId, size ) => {
-    if(!size){
-      toast.error("Please select a size before adding to cart");  
+  const addToCart = async (itemId, size) => {
+    if (!size) {
+      toast.error("Please select a size before adding to cart");
       return;
-
     }
     let cartData = structuredClone(cartItems);
 
@@ -36,42 +33,44 @@ const ShopContextProvider = (props) => {
     setCartItems(cartData);
   };
 
-
   const getCartCount = () => {
     let totalCount = 0;
-    for(const items in cartItems){
-      for(const item in cartItems[items]){
-       try{
-
-         if(cartItems[items][item]>0){
-
-          totalCount += cartItems[items][item];
-
-         }
-
-       }catch(e){
-
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalCount += cartItems[items][item];
+          }
+        } catch (e) {}
       }
     }
-   
+    return totalCount;
+  };
 
-  }
-   return totalCount;
+  const updateQuantity = async (itemId, size, quantity) => {
+    let cartData = structuredClone(cartItems);
 
-}
+    cartData[itemId][size] = quantity;
 
-const updateQuantity = async(itemId, size, quantity) => {
+    setCartItems(cartData);
+  };
 
-  let cartData = structuredClone(cartItems);
+  const getCartAmount = () => {
+    let totalAmount = 0;
+    for (const items in cartItems) {
+      let itemInfo = products.find((product) => product._id === items);
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalAmount += itemInfo.price * cartItems[items][item];
+          }
+        } catch (error) {
 
-
-  cartData[itemId][size] = quantity;
-
-  setCartItems(cartData);
-
-};
-
-
+        }
+      }
+    }
+    return totalAmount;
+  };
 
   const value = {
     products,
@@ -84,7 +83,8 @@ const updateQuantity = async(itemId, size, quantity) => {
     cartItems,
     addToCart,
     getCartCount,
-    updateQuantity
+    updateQuantity,
+    getCartAmount
   };
 
   return (
