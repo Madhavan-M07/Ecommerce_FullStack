6491149b01c59bd15f444307c9eead299b4d93ca
yokesh <yokesh.s@gus.global>
2026-01-22 00:02:35 +0530

    feat:contact page creation and frontend pages all the creation done

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Title from "../components/Title";
 import { assets } from "../assets/assets";
+import Newsletterbox from "../components/NewsletterBox";
 
 const About = () => {
   return (
@@ -39,7 +40,30 @@ const About = () => {
       <div className="text-xl py-4 ">
         <Title text1={"WHY "} text2={" CHOOSE US"} />
       </div>
-      
+      <div className="flex flex-col md:flex-row text-sm mb-20 ">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Quality Assurance:</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+            repellat accusantium cum, cupiditate officia doloribus dolorum atque.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Convenience:</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+            repellat accusantium cum, cupiditate officia doloribus dolorum atque.
+          </p>
+        </div>
+           <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Exceptional Customer Service:</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+            repellat accusantium cum, cupiditate officia doloribus dolorum atque.
+          </p>
+        </div>
+      </div>
+      <Newsletterbox />
     </div>
   );
 };
