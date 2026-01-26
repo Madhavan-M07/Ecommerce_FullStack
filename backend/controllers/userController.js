0664c7f1e1398080c4d01315b27d5ecbbb8415ed
yokesh <yokesh.s@gus.global>
2026-01-26 22:05:30 +0530

    feat:backend product api done at 65819

---
+++ backend/controllers/userController.js
@@ -0,0 +1,105 @@
+import express from "express";
+import userModel from "../models/userModel.js";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
+console.log(process.env.JWT_SECRET);
+
+// Route for User Login
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.json({ success: "false", msg: "User does not exists" });
+    }
+
+    const isMatch = await bcrypt.compare(password,user.password)
+
+    if(isMatch){
+
+        const token = createToken(user._id);
+        res.json({
+            success:true,
+            token,
+        })
+    } else{
+
+        res.json({success:"false",msg:"Invalid Credentials"})
+
+    }
+
+  } catch (error) {
+
+     console.log(error);
+     res.json({success:false,message:error.message})
+  }
+};
+
+// Route for User Registeration
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // checking user alredy exists or not
+
+    const exists = await userModel.findOne({ email });
+    if (exists) {
+      return res.json({ success: "false", msg: "User already exists" });
+    }
+
+    // validating email format and strong format
+
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: "false",
+        msg: "Please Enter the Valid Email",
+      });
+    }
+
+    if (password.length < 8) {
+      return res.json({
+        success: "false",
+        msg: "Please Enter the Strong Password",
+      });
+    }
+
+    // hashing user password
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+
+    const token = createToken(user._id);
+    console.log(token);
+
+    res.json({
+      success: true,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Route for Admin Login
+
+const loginAdmin = async (req, res) => {};
+
+export { loginUser, registerUser, loginAdmin };
---
+++ backend/server.js
@@ -0,0 +1,37 @@
+import express from "express"
+import cors from  'cors'
+import 'dotenv/config'
+import { connect } from "mongoose"
+import connectDB from "./config/mongodb.js"
+import connectCloudinary from "./config/cloudinary.js"
+import userRouter from "./routes/userRoute.js"
+import productRouter from "./routes/productRoute.js"
+
+
+
+
+// App Config
+
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+// middlewares 
+app.use(express.json())
+app.use(cors())
+
+// API endpoint
+
+app.use('/api/user',userRouter)
+app.use('/api/product',productRouter)
+
+
+
+app.get('/',(req,res)=>{
+    res.send("API WORKING")
+})
+
+app.listen(port,()=>
+console.log(`Listening on localhost:${port}`)
+)
