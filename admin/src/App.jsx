d3073737a8f40e3a0a25747ffc80895554fa1a05
yokesh <yokesh.s@gus.global>
2026-01-30 01:42:53 +0530

    admin panel login screen auth handled

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,26 +1,48 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Sidebar from './components/Sidebar'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Orders from "./pages/Orders";
+import Login from "./components/Login";
+import { useState } from "react";
+ import { ToastContainer } from 'react-toastify';
+ import 'react-toastify/dist/ReactToastify.css';
+
+
+
+
+export const backendURL = import.meta.env.VITE_BACKEND_URL;
 
 const App = () => {
+  const [token, setToken] = useState("");
+
   return (
-    <div className='bg-gray-50 min-h-screen'>
+    <div className="bg-gray-50 min-h-screen">
+      <ToastContainer />
+      {token === "" ? (
+        <Login setToken={setToken} />
+      ) : (
+        <>
+          <Navbar />
+          <hr />
+          <div className="flex w-full">
+            {/* Sidebar */}
 
-     <>
-      <Navbar />
-      <hr />
-     </>
-     <div className='flex w-full'>
-      {/* Sidebar */}
-
-      <Sidebar />
-
-     </div>
-    
-
-
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-800 text-base ">
+              <Routes>
+                <Route path="/add" element={<Add />} />
+                <Route path="/list" element={<List />} />
+                <Route path="/orders" element={<Orders />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
